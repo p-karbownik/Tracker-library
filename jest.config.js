20561cd28bdc5b202c9a06bdd0660f04be9723ae
Patryk Karbownik <patryk.karbownik98@gmail.com>
2021-04-05 15:57:22 +0200

    modified jest configuration

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,21 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  reporters: [
+    'default',
+    [
+      'jest-junit',
+      {
+        outputName: 'output/coverage/junit/junit.xml',
+      },
+    ],
+  ],
+  coverageThreshold: {
+    global: {
+      statements: 50,
+      branches: 90,
+      functions: 0,
+      lines: 0,
+    },
+  },
 };
