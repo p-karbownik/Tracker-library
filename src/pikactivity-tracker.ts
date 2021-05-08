0c57d1a4339122c83834b150e4d3498bb95b463d
aksek <kosekaniela@gmail.com>
2021-05-08 07:47:55 +0200

    relocated send as a method of Event

--- src/pikactivity-tracker.ts
+++ src/pikactivity-tracker.ts
@@ -2,10 +2,6 @@ export function trackerInitialise() {
     console.log('Not implemented yet!')
 }
 
-export function sendEvent() {
-    console.log('Not implemented yet!')
-}
-
 export function enableEventTracking() {
     console.log('Not implemented yet!')
 }
